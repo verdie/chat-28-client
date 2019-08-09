5c85224a19e91d53254b394533c3a2f7e16324e8
halynaburdiian <verdamtion@gmail.com>
2019-08-09 12:28:14 +0200

    fix superagent bug

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import * as request from 'superagent'
 
 class App extends React.Component{
   state={
+    message: '',
     messages: []
   }
   source = new EventSource('http://localhost:5000/stream')
   componentDidMount(){
     this.source.onmessage =  (event) => {
       console.log('event test:', event)
-      const messages = JSON.parse(event.data)
+      const messages =JSON.parse(event.data)
       this.setState({messages})
+      console.log(messages)
+      console.log(this.state)
     }
   }
 
@@ -19,9 +22,9 @@ class App extends React.Component{
     event.preventDefault()
 
     await request
-      .post('http://localhost/message')
+      .post('http://localhost:5000/message')
       .send({message: this.state.message})
-
+    console.log('STATE:', this.state.message)
     this.setState({messages: ''})
 
     
@@ -35,7 +38,7 @@ class App extends React.Component{
     render(){
       const messages = this.state.messages.map((message,index)=> <p key={index}>{message}</p>)
       const form =<form onSubmit= {this.onSubmit}>
-        <input type='text' value = {this.state.message}/>
+        <input type='text' value = {this.state.message} onChange={this.onChange}/>
         <button type='submit'>Send</button>
       </form>
       return <main>{form}{messages}</main>
