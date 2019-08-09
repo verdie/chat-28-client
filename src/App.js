8e61364d203eaf9a1d89c64f40ace93a39a65364
halynaburdiian <verdamtion@gmail.com>
2019-08-09 11:43:03 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  state={
+    messages: []
+  }
+  source = new EventSource('http://localhost:5000/stream')
+  componentDidMount(){
+    this.source.onmessage =  (event) => {
+      console.log('event test:', event)
+      const newMessages= [...this.state.messages, event.data]
+      this.setState({messages: newMessages})
+    }
+  }
+    
+    render(){
+      const messages = this.state.messages.map(message=> <p>{message}</p>)
+      const form =<form onSubmit= {this.onSubmit}>
+        <input type='text'/>
+        <button type='submit'>Send</button>
+      </form>
+      return <main>{form}{messages}</main>
+    }
 }
-
 export default App;
